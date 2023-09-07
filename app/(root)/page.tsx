5ca27b90379fe5afdec26f7c13558c252a6937e1
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-07 14:40:06 +0600

    reusable modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from '@/components/ui/button';
+'use client';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <div className='p-4'>
-      <Button>Click Me</Button>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <main className='p-4'>Root Page</main>;
 }
