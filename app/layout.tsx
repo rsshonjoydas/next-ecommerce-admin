0b620e0d9c610941a80e4c52e017ac0cd161cabf
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-08 08:18:08 +0600

    react hot toast setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
-import { ModalProvider } from '@/providers/modal-provider';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import { ModalProvider } from '@/providers/modal-provider';
+import { ToastProvider } from '@/providers/toast-provider';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,6 +23,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
